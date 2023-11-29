98b4b8bfe5166ba3f5d5745142285984e41736f3
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2023-11-28 21:38:34 -0600

    Preguntas FAQ terminadas

--- app/src/main/java/com/example/winestastic/chatbot.java
+++ app/src/main/java/com/example/winestastic/chatbot.java
@@ -43,12 +43,13 @@ public class chatbot extends AppCompatActivity {
 
         versionsList = new ArrayList<>();
 
-        versionsList.add(new Versions("Primer pregunta frecuente", "Aqui ira la primer descripcion de la pregunta frecuente que depende la respuesta estara el texto"));
-        versionsList.add(new Versions("Segunda pregunta frecuente", "Aqui ira la primer descripcion de la pregunta frecuente que depende la respuesta estara el texto"));
-        versionsList.add(new Versions("Hola", "ajasldfjasldfjsaldfjksdfljsdffljk"));
-        versionsList.add(new Versions("Estoy cansado jefe", "estoy cansado de tanto correr en la nieve como un gorrion asustado"));
-        versionsList.add(new Versions("Esto es una prueba", "Realmente es un texto al azar, no le encuentro logica"));
-        versionsList.add(new Versions("Por que sigues leyendo?", "Vete a comer o algo, ya son las 3 casi las 4"));
+        versionsList.add(new Versions("¿Cómo puedo encontrar eventos en mi zona?", "Para encontrar eventos en tu zona, puedes utilizar la función de búsqueda de la app. \n" +
+                "También  puedes utilizar el mapa de la app para ver dónde se encuentran las catas que están cerca de ti. \n" +
+                "O si lo prefieres, puedes utilizar los filtros de la app para buscar eventos específicos."));
+        versionsList.add(new Versions("¿Cómo puedo reservar un lugar en un evento de cata?", "Para reservar un lugar en un evento de cata, simplemente haz clic en el evento que te interesa y luego haz clic en el botón \"Reservar\". La app te llevará a una página donde podrás introducir tus datos personales y completar el pago."));
+        versionsList.add(new Versions("¿Cuánto cuestan las catas?", "El precio de las catas varía según el tipo de cata, la ubicación y el lugar que organice el evento."));
+        versionsList.add(new Versions("¿Cómo puedo pagar mi reservación?", "Puedes pagar tu reservación con tarjeta de crédito, débito o PayPal. El pago se realiza a través de la app de forma segura y sencilla."));
+        versionsList.add(new Versions("¿Cómo puedo cancelar o modificar mi reserva?", "Si necesitas cancelar o modificar tu reserva, puedes hacerlo a través de la app. Simplemente haz clic en el botón \"Cancelar\" o \"Modificar\" que aparece junto a la reserva que deseas modificar."));
 
 
     }
