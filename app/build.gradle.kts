7b56190a2ad11012a41b1b7a3bde0347402c41e0
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2024-06-28 08:34:02 -0600

    gradle update to 8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,6 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
-    implementation ("com.google.android.gms:play-services-maps:17.0.1")
+    implementation ("com.google.android.gms:play-services-maps:17.0.1") 
 
 }
