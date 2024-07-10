60fd44e2fce74ebb4075a0984dfa8846645a90db
Christian Rios <crismancilla36@gmail.com>
2024-07-10 08:52:46 -0600

    ImageSlider created

---
+++ app/src/main/java/com/example/winestastic/Tasting.java
@@ -0,0 +1,53 @@
+package com.example.winestastic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tasting extends ImageSliderActivity {
+
+
+
+    List<Versions> versionsList;
+    protected RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //incrustar activity contact
+        NestedScrollView nscrollv;
+        nscrollv = findViewById(R.id.nestedScrollView);
+        LayoutInflater inflater = getLayoutInflater();
+        View myLayout = inflater.inflate(R.layout.activity_tasting, nscrollv, false);
+        nscrollv.removeAllViews();
+        nscrollv.addView(myLayout);
+
+    }
+
+    private void setRecyclerView() {
+        VersionsAdapter versionsAdapter = new VersionsAdapter(versionsList);
+        recyclerView.setAdapter(versionsAdapter);
+    }
+
+
+
+}
