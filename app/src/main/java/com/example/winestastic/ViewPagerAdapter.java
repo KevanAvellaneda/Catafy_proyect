948a4fb2b0e5efb1fbecafff1d473ca8aeee9626
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2024-07-31 13:11:11 -0600

    add rounded corner in shapeable image

--- app/src/main/java/com/example/winestastic/ViewPagerAdapter.java
+++ app/src/main/java/com/example/winestastic/ViewPagerAdapter.java
@@ -9,8 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.imageview.ShapeableImageView;
 
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.ViewHolder> {
 
@@ -33,9 +39,20 @@ public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.View
         holder.textViewTitle.setText(pageData.getTitle());
         holder.textViewDescription.setText(pageData.getDescription());
 
+        // Configuración de resolucion para Glide
+        RequestOptions requestOptions = new RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .override(400,400);
+
         // Usar Glide para cargar la imagen desde la URL
         Glide.with(holder.itemView.getContext())
+                .asBitmap() // Cargar como un bitmap para la carga progresiva
                 .load(pageData.getImageUrl())
+                .thumbnail(0.20f)
+                .placeholder(R.drawable.cargandoo) // Cargamos una imagen de baja resolución inicialmente
+                .error(R.drawable.errorr) //Imagen en caso de error al cargar
+                .apply(requestOptions) // Aplicar opciones de cache
+                .transition(BitmapTransitionOptions.withCrossFade()) // Agregar transición al cargar la imagen
                 .into(holder.imageView);
     }
 
@@ -45,7 +62,7 @@ public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.View
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
+        ShapeableImageView imageView;
         TextView textViewTitle;
         TextView textViewDescription;
 
